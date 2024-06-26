df04c92ccc3fb56547f33cedfaac16aed880a8e3
Fredrik Lian <fredrik@lianfjell.no>
2024-06-26 22:28:01 +0200

    laget franchise profil og profil for ansatte. Liste over ansatte i samme klinikk som franchise-takeren

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile/<str:username>/', views.profile_view, name='profile_view'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('franchise_taker_profile/<str:username>/', views.franchise_taker_profile_view, name='franchise_taker_profile'),
+    path('employee_profile/<str:username>/', views.employee_profile_view, name='employee_profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -29,53 +29,121 @@ def profile_view(request, username):
         user = get_object_or_404(User, username=username)
         profile = user.profile
 
-        # Antall pasienter
-        clients = Client.objects.filter(psychologist=user)
-        number_of_patients = clients.count()
-        profile.key_metrics['number_of_patients'] = number_of_patients
-
-        # Gjennomsnittlig antall avtaler per pasient
-        if number_of_patients > 0:
-            total_appointments = Appointment.objects.filter(client__in=clients).count()
-            average_appointments_per_patient = total_appointments / number_of_patients
+        if profile.role == 'franchise_taker':
+            return redirect('franchise_taker_profile', username=username)
         else:
-            average_appointments_per_patient = 0
-        profile.key_metrics['average_appointments_per_patient'] = average_appointments_per_patient
-
-        # Beregn gjennomsnittlig antall pasienter per ukedag de siste 30 dagene (mandag til fredag)
-        start_date = now() - timedelta(days=30)
-        appointments_last_30_days = Appointment.objects.filter(client__in=clients, date__gte=start_date)
-        
-        weekday_counts = [0] * 5  # Mandag til fredag
-        for appointment in appointments_last_30_days:
-            weekday = appointment.date.weekday()
-            if weekday < 5:  # Mandag til fredag
-                weekday_counts[weekday] += 1
-
-        total_weekdays = (now().date() - start_date.date()).days
-        num_weeks = total_weekdays // 7
-        extra_days = total_weekdays % 7
-
-        # Beregn antall virkedager (mandag til fredag) i de siste 30 dagene
-        num_weekdays = num_weeks * 5 + min(extra_days, 5)
-
-        if num_weekdays > 0:
-            average_patients_per_weekday = sum(weekday_counts) / num_weekdays
-        else:
-            average_patients_per_weekday = 0
-
-        # Formater til én desimal
-        profile.key_metrics['average_patients_per_weekday'] = format(average_patients_per_weekday, '.1f')
-
-        # Hent gruppene brukeren tilhører
-        groups = user.groups.all()
-
-        return render(request, 'authentication/profile.html', {'user': user, 'profile': profile, 'groups': groups})
+            return redirect('employee_profile', username=username)
 
     except Exception as e:
         # Logg feilen og returner en melding
         print(f"Error in profile_view: {e}")
         return render(request, 'authentication/profile.html', {'error': 'An error occurred while retrieving the profile.'})
+
+@login_required
+def franchise_taker_profile_view(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = user.profile
+
+    # Antall pasienter
+    clients = Client.objects.filter(psychologist=user)
+    number_of_patients = clients.count()
+    profile.key_metrics['number_of_patients'] = number_of_patients
+
+    # Gjennomsnittlig antall avtaler per pasient
+    if number_of_patients > 0:
+        total_appointments = Appointment.objects.filter(client__in=clients).count()
+        average_appointments_per_patient = total_appointments / number_of_patients
+    else:
+        average_appointments_per_patient = 0
+    profile.key_metrics['average_appointments_per_patient'] = average_appointments_per_patient
+
+    # Gjennomsnittlig antall pasienter per ukedag de siste 30 dagene (mandag til fredag)
+    start_date = now() - timedelta(days=30)
+    appointments_last_30_days = Appointment.objects.filter(client__in=clients, date__gte=start_date)
+    
+    weekday_counts = [0] * 5  # Mandag til fredag
+    for appointment in appointments_last_30_days:
+        weekday = appointment.date.weekday()
+        if weekday < 5:  # Mandag til fredag
+            weekday_counts[weekday] += 1
+
+    total_weekdays = (now().date() - start_date.date()).days
+    num_weeks = total_weekdays // 7
+    extra_days = total_weekdays % 7
+
+    # Beregn antall virkedager (mandag til fredag) i de siste 30 dagene
+    num_weekdays = num_weeks * 5 + min(extra_days, 5)
+
+    if num_weekdays > 0:
+        average_patients_per_weekday = sum(weekday_counts) / num_weekdays
+    else:
+        average_patients_per_weekday = 0
+
+    # Formater til én desimal
+    profile.key_metrics['average_patients_per_weekday'] = format(average_patients_per_weekday, '.1f')
+
+    # Hent gruppene brukeren tilhører
+    groups = user.groups.all()
+
+    # Hent ansatte som tilhører samme gruppe
+    employees = User.objects.filter(groups__in=groups).exclude(id=user.id)
+
+    return render(request, 'authentication/franchise_taker_profile.html', {
+        'user': user,
+        'profile': profile,
+        'groups': groups,
+        'employees': employees
+    })
+
+
+
+@login_required
+def employee_profile_view(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = user.profile
+
+    # Antall pasienter
+    clients = Client.objects.filter(psychologist=user)
+    number_of_patients = clients.count()
+    profile.key_metrics['number_of_patients'] = number_of_patients
+
+    # Gjennomsnittlig antall avtaler per pasient
+    if number_of_patients > 0:
+        total_appointments = Appointment.objects.filter(client__in=clients).count()
+        average_appointments_per_patient = total_appointments / number_of_patients
+    else:
+        average_appointments_per_patient = 0
+    profile.key_metrics['average_appointments_per_patient'] = average_appointments_per_patient
+
+    # Gjennomsnittlig antall pasienter per ukedag de siste 30 dagene (mandag til fredag)
+    start_date = now() - timedelta(days=30)
+    appointments_last_30_days = Appointment.objects.filter(client__in=clients, date__gte=start_date)
+    
+    weekday_counts = [0] * 5  # Mandag til fredag
+    for appointment in appointments_last_30_days:
+        weekday = appointment.date.weekday()
+        if weekday < 5:  # Mandag til fredag
+            weekday_counts[weekday] += 1
+
+    total_weekdays = (now().date() - start_date.date()).days
+    num_weeks = total_weekdays // 7
+    extra_days = total_weekdays % 7
+
+    # Beregn antall virkedager (mandag til fredag) i de siste 30 dagene
+    num_weekdays = num_weeks * 5 + min(extra_days, 5)
+
+    if num_weekdays > 0:
+        average_patients_per_weekday = sum(weekday_counts) / num_weekdays
+    else:
+        average_patients_per_weekday = 0
+
+    # Formater til én desimal
+    profile.key_metrics['average_patients_per_weekday'] = format(average_patients_per_weekday, '.1f')
+
+    # Hent gruppene brukeren tilhører
+    groups = user.groups.all()
+
+    return render(request, 'authentication/employee_profile.html', {'user': user, 'profile': profile, 'groups': groups})
 
 @login_required
 def edit_profile(request):
